1f1cd25ff6f5e043f4ccbdedb54073ac04c966d0
rupamondal123 <rupamondaljiaganj123@gmail.com>
2026-03-14 15:27:29 +0530

    resolved conflict

--- manage.py
+++ manage.py
@@ -6,11 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-<<<<<<< HEAD
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logistics.settings')
-=======
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'logestics.settings')
->>>>>>> afec8438f19ed7e8aecde76a5ff0f7d6dda38b82
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
